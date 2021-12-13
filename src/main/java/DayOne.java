992c9fd5e792df27a593d46eb38dabdb7e6f1d6e
Styrfire <g_n_rogers1991@yahoo.com>
2021-12-13 17:39:35 -0600

    Initial Commit (Broken!)

---
+++ src/main/java/DayOne.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class DayOne
+{
+	public static void main(String[] args)
+	{
+		File input = ;
+		try (BufferedReader br = new BufferedReader(new FileReader(input))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				// process the line.
+			}
+		}
+	}
+}
